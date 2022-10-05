c402442c18ceb859ac43d0426296f3d4ef96b5c7
Alexandre Lipari <santuslipari@gmail.com>
2022-10-04 21:04:24 -0300

    Inicindo primeiro exercício, tempo esgotado

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+let listaNumerosA = [2, 6, 9, 7, 5, 3, 1, 8, 10, 11];
+let listaNumerosB = [];
+let listaNumerosC = [];
+listaNumerosA.forEach(x => );
--- script.ts
+++ script.ts
@@ -1,2 +1,4 @@
-console.log("Olá Mundo!")
-//teste
+let listaNumerosA: number [] = [2,6,9,7,5,3,1,8,10,11]
+let listaNumerosB: number [] = []
+let listaNumerosC: number [] = []
+listaNumerosA.forEach(x =>)
