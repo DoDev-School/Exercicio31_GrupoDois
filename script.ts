f5abcb956b66f1b1bf6257f0ec0fb745ccf2ecf3
Alexandre Lipari <santuslipari@gmail.com>
2022-10-04 22:58:28 -0300

    Exercicio tres terminando codigo

--- script.ts
+++ script.ts
@@ -1,32 +1,43 @@
 
 
-// let listaNumerosA: number[] = [2, 6, 9, 7, 5, 3, 1, 8, 10, 11];
-// let listaNumerosB: number[] = [];
-// let listaNumerosC: number[] = [];
+// // let listaNumerosA: number[] = [2, 6, 9, 7, 5, 3, 1, 8, 10, 11];
+// // let listaNumerosB: number[] = [];
+// // let listaNumerosC: number[] = [];
 
-// listaNumerosA.forEach(Verificate);
+// // listaNumerosA.forEach(Verificate);
 
-// console.log(listaNumerosA);
-// console.log(listaNumerosB);
-// console.log(listaNumerosC);
+// // console.log(listaNumerosA);
+// // console.log(listaNumerosB);
+// // console.log(listaNumerosC);
 
-// function Verificate(item: number): void {
-//   if (item % 2 == 0) {
-//     listaNumerosB.push(item);
-//   } else {
-//     listaNumerosC.push(item);
+// // function Verificate(item: number): void {
+// //   if (item % 2 == 0) {
+// //     listaNumerosB.push(item);
+// //   } else {
+// //     listaNumerosC.push(item);
+// //   }
+// // }
+
+// let arrayA: number[] = [5, 6, 2, 4, 8, 7, 3, 6, 7, 1],
+//   arrayB: number[] = [8, 5, 3, 1, 4, 7, 8, 2, 3, 6],
+//   arrayC: number[] = [];
+
+// arrayA.forEach(check);
+
+// function check(item: number, index: number): void {
+//   if (arrayA.includes(arrayB[index]) && arrayB[index] >= 5) {
+//     arrayC.push(arrayB[index]);
 //   }
 // }
+// console.log(`ArrayA: ${arrayA} \nArrayB: ${arrayB} \nArrayC: ${arrayC}`);
 
-let arrayA: number[] = [5, 6, 2, 4, 8, 7, 3, 6, 7, 1],
-  arrayB: number[] = [8, 5, 3, 1, 4, 7, 8, 2, 3, 6],
-  arrayC: number[] = [];
+let alunos: string[] = []
+class Aluno {
+  Nome: string;
+  Idade: number;
+  Altura: number;
+  Sexo: string;
+  constructor()
+  
 
-arrayA.forEach(check);
-
-function check(item: number, index: number): void {
-  if (arrayA.includes(arrayB[index]) && arrayB[index] >= 5) {
-    arrayC.push(arrayB[index]);
-  }
 }
-console.log(`ArrayA: ${arrayA} \nArrayB: ${arrayB} \nArrayC: ${arrayC}`);
