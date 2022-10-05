20d0fc2152c3942e1821bd3c4a902309b75dca77
Luiz Pachioni <lopachioni@gmail.com>
2022-10-04 21:41:24 -0300

    atualizando

--- script.ts
+++ script.ts
@@ -1,2 +1,21 @@
-console.log("Olá Mundo!")
-//teste
+let listaNumerosA: number [] = [2, 6, 9, 7, 5, 3, 1, 8, 10, 11]
+let listaNumerosB: number [] = []
+let listaNumerosC: number [] = []
+
+
+listaNumerosA.forEach(Verificate)
+
+console.log(listaNumerosA)
+console.log(listaNumerosB)
+console.log(listaNumerosC)
+
+function Verificate(item: number): void{
+if(item % 2 == 0){
+listaNumerosB.push(item)
+}
+
+else{
+listaNumerosC.push(item)
+}
+
+}
