2fc6a94ec4533ebc2c4e0da64a75c37bf85f685c
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-04 22:22:28 -0300

    2 Exercicio Finalizado

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-
-
 // let listaNumerosA: number[] = [2, 6, 9, 7, 5, 3, 1, 8, 10, 11];
 // let listaNumerosB: number[] = [];
 // let listaNumerosC: number[] = [];
-
 // listaNumerosA.forEach(Verificate);
-
 // console.log(listaNumerosA);
 // console.log(listaNumerosB);
 // console.log(listaNumerosC);
-
 // function Verificate(item: number): void {
 //   if (item % 2 == 0) {
 //     listaNumerosB.push(item);
@@ -17,16 +12,11 @@
 //     listaNumerosC.push(item);
 //   }
 // }
-
-let arrayA: number[] = [5, 6, 2, 4, 8, 7, 3, 6, 7, 1],
-  arrayB: number[] = [8, 5, 3, 1, 4, 7, 8, 2, 3, 6],
-  arrayC: number[] = [];
-
+var arrayA = [5, 6, 2, 4, 8, 7, 3, 6, 7, 1], arrayB = [8, 5, 3, 1, 4, 7, 8, 2, 3, 6], arrayC = [];
 arrayA.forEach(check);
-
-function check(item: number, index: number): void {
-  if (arrayA.includes(arrayB[index]) && arrayB[index] >= 5) {
-    arrayC.push(arrayB[index]);
-  }
+function check(item, index) {
+    if (arrayA.includes(arrayB[index]) && arrayB[index] >= 5) {
+        arrayC.push(arrayB[index]);
+    }
 }
-console.log(`ArrayA: ${arrayA} \nArrayB: ${arrayB} \nArrayC: ${arrayC}`);
+console.log("ArrayA: ".concat(arrayA, " \nArrayB: ").concat(arrayB, " \nArrayC: ").concat(arrayC));
